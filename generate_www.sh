ed1ed584d510425611241301f72bc94b3d509bdf
Tim Lossen <tim@lossen.de>
2009-12-17 14:20:17 +0100

    only one settings file for all charts

--- generate_www.sh
+++ generate_www.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-wget -r http://localhost:8000
-cp -r localhost\:8000/* www
-rm -rf localhost\:8000
+wget -r http://localhost:4567
+mkdir -p www
+cp -r localhost\:4567/* www
+rm -rf localhost\:4567
 cp www/charts/eu-west-1.linux.m1.small.html www/index.html

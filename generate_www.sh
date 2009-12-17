abc123c0d8c3d05b4bcfdca4e2aa211017f8f4cb
Tim Lossen <tim@lossen.de>
2009-12-17 10:04:30 +0100

    improved spidering

--- generate_www.sh
+++ generate_www.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-wget -r -i files.txt
+wget -r http://localhost:8000
 cp -r localhost\:8000/* www
 rm -rf localhost\:8000
 cp www/charts/eu-west-1.linux.m1.small.html www/index.html

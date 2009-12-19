63dfb819bb701425497a3954fe1dbe30870f6bf3
Tim Lossen <tim@lossen.de>
2009-12-19 22:20:13 +0100

    calculate percentage of regular price

--- fetch_history.rb
+++ fetch_history.rb
@@ -2,29 +2,69 @@
 
 require 'time'
 
-#DATA_DIR = "/home/ubuntu/cloudexchange.org/www/data"
-DATA_DIR = "/Users/tlossen/Code/cloudexchange.org/public/data"
+DATA_DIR = "/home/ubuntu/cloudexchange.org/www/data"
+#DATA_DIR = "/Users/tim/Code/cloudexchange.org/public/data"
 
+REGULAR_PRICE = {
+  'eu-west-1.linux.m1.small' => 0.095,
+  'eu-west-1.linux.m1.large' => 0.38,
+  'eu-west-1.linux.m1.xlarge' => 0.76,
+  'eu-west-1.linux.m2.2xlarge' => 1.34,
+  'eu-west-1.linux.m2.4xlarge' => 2.68,
+  'eu-west-1.linux.c1.medium' => 0.19,
+  'eu-west-1.linux.c1.xlarge' => 0.76,
+  'eu-west-1.windows.m1.small' => 0.12,
+  'eu-west-1.windows.m1.large' => 0.48,
+  'eu-west-1.windows.m1.xlarge' => 0.96,
+  'eu-west-1.windows.m2.2xlarge' => 1.44,
+  'eu-west-1.windows.m2.4xlarge' => 2.88,
+  'eu-west-1.windows.c1.medium' => 0.29,
+  'eu-west-1.windows.c1.xlarge' => 1.16,
+  'us-east-1.linux.m1.small' => 0.085,
+  'us-east-1.linux.m1.large' => 0.34,
+  'us-east-1.linux.m1.xlarge' => 0.68,
+  'us-east-1.linux.m2.2xlarge' => 1.20,
+  'us-east-1.linux.m2.4xlarge' => 2.40,
+  'us-east-1.linux.c1.medium' => 0.17,
+  'us-east-1.linux.c1.xlarge' => 0.68,
+  'us-east-1.windows.m1.small' => 0.12,
+  'us-east-1.windows.m1.large' => 0.48,
+  'us-east-1.windows.m1.xlarge' => 0.96,
+  'us-east-1.windows.m2.2xlarge' => 1.44,
+  'us-east-1.windows.m2.4xlarge' => 2.88,
+  'us-east-1.windows.c1.medium' => 0.29,
+  'us-east-1.windows.c1.xlarge' => 1.16,
+  'us-west-1.linux.m1.small' => 0.095,
+  'us-west-1.linux.m1.large' => 0.38,
+  'us-west-1.linux.m1.xlarge' => 0.76,
+  'us-west-1.linux.m2.2xlarge' => 1.34,
+  'us-west-1.linux.m2.4xlarge' => 2.68,
+  'us-west-1.linux.c1.medium' => 0.19,
+  'us-west-1.linux.c1.xlarge' => 0.76,
+  'us-west-1.windows.m1.small' => 0.13,
+  'us-west-1.windows.m1.large' => 0.52,
+  'us-west-1.windows.m1.xlarge' => 1.04,
+  'us-west-1.windows.m2.2xlarge' => 1.58,
+  'us-west-1.windows.m2.4xlarge' => 3.16,
+  'us-west-1.windows.c1.medium' => 0.31,
+  'us-west-1.windows.c1.xlarge' => 1.24,
+}
 
 def store(region, data)
-  # open("#{DATA_DIR}/#{region}.csv", 'w') do |file|
-  #   data.each do |type, values|
-  #     file.puts "#{type},#{values.last[1]}"
-  #   end
-  # end
-
   data.keys.each do |type|
-    open("#{DATA_DIR}/#{region}.#{type}.csv", 'w') do |file|
+    which = "#{region}.#{type}"
+    open("#{DATA_DIR}/#{which}.csv", 'w') do |file|
       data[type].each do |stamp, price|
         file.puts "#{stamp.strftime('%Y-%m-%d %H:%M:%S')},#{price}"
       end
     end
-    open("#{DATA_DIR}/#{region}.#{type}.txt", 'w') do |file|
-      file.puts data[type].last[1]
+    price = data[type].last[1]
+    percent = (price / REGULAR_PRICE[which] * 100).round
+    open("#{DATA_DIR}/#{which}.txt", 'w') do |file|
+      file.puts "#{'%.3f' % price} (#{percent}%)"
     end
   end
 end
-
 
 def fetch(region)
   puts "#{Time.now} - fetching #{region}"

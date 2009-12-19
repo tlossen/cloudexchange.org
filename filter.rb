d92f9f3aa7b48c212ca2518d1b9dcecc85beaf58
Tim Lossen <tim@lossen.de>
2009-12-19 23:17:13 +0100

    user agent is cyan

--- filter.rb
+++ filter.rb
@@ -12,11 +12,16 @@ def green(text)
   colorize(text, "\e[32m")
 end
 
+def cyan(text)
+  colorize(text, "\e[36m")
+end
+
 #line = %q{85.179.21.217 - - [19/Dec/2009:22:37:02 +0100] "GET /charts/eu-west-1.windows.m1.small.html HTTP/1.1" 200 1826 "http://www.cloudexchange.org/charts/us-west-1.windows.m1.small.html" "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5"}
 
 while line = $stdin.readline do
-  parts = line.split
+  parts = line.split(' ', 12)
   parts[0] = red(parts[0])
   parts[10] = green(parts[10])
+  parts[11] = cyan(parts[11])
   $stdout.puts parts.join(" ")
 end

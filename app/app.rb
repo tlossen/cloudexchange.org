808440fa99e5e535f10dd8124ab782acb2c7a08d
Tim Lossen <tim@lossen.de>
2010-01-01 13:11:16 +0100

    startseite statt redirect

--- app/app.rb
+++ app/app.rb
@@ -7,17 +7,19 @@ SYSTEMS = ['linux', 'windows']
 INSTANCES = ['m1.small', 'm1.large', 'm1.xlarge', 'c1.medium', 'c1.xlarge', 'm2.2xlarge', 'm2.4xlarge']
 
 get '/' do
-  redirect '/charts/eu-west-1.linux.m1.small.html'
-end
-
-get '/check' do
-  'ok'
+  @which = 'eu-west-1.linux.m1.small'
+  @spot_price = spot_price_map
+  erb :chart
 end
 
 get '/charts/:which.html' do
   @which = params[:which]
   @spot_price = spot_price_map
   erb :chart
+end
+
+get '/check' do
+  'ok'
 end
 
 def spot_price_map

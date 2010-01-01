f217816202ea80fa2deadd65193b07cf43d353cf
Tim Lossen <tim@lossen.de>
2010-01-01 14:26:18 +0100

    refactoring: sinatra helpers

--- app/app.rb
+++ app/app.rb
@@ -8,13 +8,11 @@ INSTANCES = ['m1.small', 'm1.large', 'm1.xlarge', 'c1.medium', 'c1.xlarge', 'm2.
 
 get '/' do
   @which = 'eu-west-1.linux.m1.small'
-  @spot_price = spot_price_map
   erb :chart
 end
 
 get '/charts/:which.html' do
   @which = params[:which]
-  @spot_price = spot_price_map
   erb :chart
 end
 
@@ -22,12 +20,24 @@ get '/check' do
   'ok'
 end
 
-def spot_price_map
-  result = {}
-  open("#{File.dirname(__FILE__)}/public/data/spot.csv").each do |line|
-    key, value = *line.split(',')
-    result[key] = value.to_f
+helpers do
+  def display(which)
+    (['ec2'] + which.split('.', 3)).join(' | ')
   end
-  result
+  
+  def spot_price(which)
+    '%.3f' % (@spot_price_map ||= spot_price_map)[which]
+  end
+
+  def spot_price_map
+    result = {}
+    open("#{File.dirname(__FILE__)}/public/data/spot.csv").each do |line|
+      key, value = *line.split(',')
+      result[key] = value.to_f
+    end
+    result
+  end
 end
+
+
 
